20edc4e77a16888a1b80628bb24e3a370e08e081
alexey02rus <101921891+alexey02rus@users.noreply.github.com>
2022-08-20 20:57:23 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arithProgression = new ArithProgression();
+            arithProgression.SetStart(10);
+            arithProgression.SetStep(2);
+            Console.WriteLine(arithProgression.GetNext());
+            Console.WriteLine(arithProgression.GetNext());
+            Console.WriteLine(arithProgression.GetNext());
+            arithProgression.Reset();
+            Console.WriteLine(arithProgression.GetNext());
+            Console.WriteLine(arithProgression.GetNext());
+            Console.WriteLine();
+            GeomProgression geomProgression = new GeomProgression();
+            geomProgression.SetStart(10);
+            geomProgression.SetStep(2);
+            Console.WriteLine(geomProgression.GetNext());
+            Console.WriteLine(geomProgression.GetNext());
+            Console.WriteLine(geomProgression.GetNext());
+            geomProgression.Reset();
+            Console.WriteLine(geomProgression.GetNext());
+            Console.WriteLine(geomProgression.GetNext());
+            Console.ReadKey();
+        }
+    }
+}
